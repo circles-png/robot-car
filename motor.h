c88c8a5e04392700d8cb82ff2882544073567187
Angus Mason <angus.mason@me.com>
2024-04-10 14:07:46 +1000

    comment

--- motor.h
+++ motor.h
@@ -5,13 +5,13 @@ public:
     Motor(int pin1, int pin2, int enable); // Declares the pin and enable integers
     int pin1; // Connection on the left terminal block of the motor
     int pin2; // Connection on the right terminal block of the motor
-    int enable;
-    void forward();
-    void backward();
-    void stop();
+    int enable; // Turns the motor on or off, used for speed control
+    void forward(); // Moves the motor forward
+    void backward(); // Moves the motor backward
+    void stop(); // Stops motor
     void setSpeed(int speed);
     int getSpeed();
 
 private:
-    int speed = 0;
+    int speed = 0; // Sets inital speed to zero
 };
