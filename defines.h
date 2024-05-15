41a3bfa9948fc43b7537e5639d4e4eab2cda3915
Angus Mason <angus.mason@me.com>
2024-05-15 10:26:41 +1000

    #include errors detected. Please update your includePath. Squiggles are disabled for this translation unit (/Users/angusmason/Documents/School/IST/robot-car/task-II/task-II.ino).

---
+++ defines.h
@@ -0,0 +1,12 @@
+// Define the pins
+#define ENA 5
+#define ENB 6
+#define MOTOR_A_1 10
+#define MOTOR_A_2 11
+#define MOTOR_B_1 8
+#define MOTOR_B_2 9
+#define LEFT_LINE_FINDER 7
+#define RIGHT_LINE_FINDER 4
+#define ULTRASONIC_ECHO A1
+#define ULTRASONIC_TRIGGER A0
+#define IR_RECEIVER 1
--- task-II/task-II.ino
+++ task-II/task-II.ino
@@ -1,6 +1,5 @@
 // Define the pins for the ultrasonic sensor
-#define ULTRASONIC_ECHO A0
-#define ULTRASONIC_TRIGGER A1
+#include "defines.h"
 
 void setup()
 {
