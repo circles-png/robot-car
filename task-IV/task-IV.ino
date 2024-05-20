f2445c1f5ad66229d2a8d6c5cd715e7524c7d519
Matthew Li <matthew.li10@education.nsw.gov.au>
2024-05-20 10:30:49 +1000

    Refactor distance printing format and add comments in task-IV.ino

--- task-IV/task-IV.ino
+++ task-IV/task-IV.ino
@@ -41,9 +41,10 @@ void loop()
 
     // Print a model of the car to the serial monitor
     Serial.print("[--] <- ");
+    // Print the distance to the serial monitor
     Serial.print(distanceCentimetres);
     Serial.print(" cm ");
-    // Print some more dashes
+    // Print some dashes
     for (int i = 0; i < distanceCentimetres / 10; i++)
     {
         Serial.print("-- ");
